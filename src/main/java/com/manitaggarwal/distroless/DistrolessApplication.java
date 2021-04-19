228f2c6253eab28c859b38a7fe66484ec1fde12e
Manit Aggarwal <manitaggarwal@gmail.com>
2021-04-19 22:00:59 +0530

    initial commit

---
+++ src/main/java/com/manitaggarwal/distroless/DistrolessApplication.java
@@ -0,0 +1,13 @@
+package com.manitaggarwal.distroless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistrolessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistrolessApplication.class, args);
+	}
+
+}
